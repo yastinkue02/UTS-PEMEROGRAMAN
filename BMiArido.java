06aaa1ed98b44d517bd0aa19e7075af2d90e615c
yastinkue02 <152510877+yastinkue02@users.noreply.github.com>
2023-11-30 21:40:46 +0700

    Add files via upload

---
+++ BMiArido.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bmiarido;
+
+public class BMiArido {
+
+    public static void main(String[] args) {
+        int beratbadan = 60;
+        float tinggibadan =1.69f;
+        float bmi=(beratbadan/(tinggibadan=tinggibadan));
+        
+        System.out.println("BMI ANDA ADALAH/t,"+bmi);
+        
+        if (bmi<18.5){
+            System.out.println("BERAT BADAN ANDA KURANG");
+        }
+        else if (bmi<24.9){
+            System.out.println("BERAT BADAN ANDA NORMAL/IDEAL");
+        }
+         else if (bmi<29.9){
+             System.out.println("BERAT BADAN ANDA LEBIH");
+         }
+         else{
+             System.out.println("OBESITAS");
+         }
+    }
+    
+    
+}
